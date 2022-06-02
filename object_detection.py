2a794e35e42e18e636d791415ff4ffed426c32a5
tal612 <67854281+tal612@users.noreply.github.com>
2022-06-03 00:48:34 +0300

    Add files via upload

--- object_detection.py
+++ object_detection.py
@@ -5,8 +5,9 @@ import urllib.request
 import numpy as np
 from cvlib.object_detection import draw_bbox
 import concurrent.futures
+import time
+import os
  
-url='http://192.168.43.172/cam-lo.jpg'
 im=None
  
 def run1():
@@ -30,10 +31,17 @@ def run1():
     cv2.destroyAllWindows()
         
 def run2():
+    url='http://192.168.43.172/cam-lo.jpg'
+
     cv2.namedWindow("detection", cv2.WINDOW_AUTOSIZE)
+    path = 'D:\check'
+    cam_index = 1
+    duration_in_sec = 5
 
     frames = []
     people_num = []
+    old_time = int(time.time())
+    os.chdir(path)
     while True:
         img_resp=urllib.request.urlopen(url)
         imgnp=np.array(bytearray(img_resp.read()),dtype=np.uint8)
@@ -42,9 +50,18 @@ def run2():
         bbox, label, conf = cv.detect_common_objects(im)
         num_of_people = len([l for l in label if l == 'person'])
         people_num.append(num_of_people)
-        print("num_of_people", num_of_people)
-        im = draw_bbox(im, bbox, label, conf)
+        # print("num_of_people", num_of_people)
+        # im2 = draw_bbox(im, bbox, label, conf)
+        if((int(time.time()) - old_time) % duration_in_sec == 0):
+            for i, frame in enumerate(frames):
+                cv2.imwrite(f"frame_{cam_index}_{i}.jpg", frame)
 
+            with open("max_ppl.txt", 'a') as file:
+                file.write(f" {str(max(people_num))}")
+            
+            frames = []
+            num_of_people = []
+        
         cv2.imshow('detection',im)
         key=cv2.waitKey(5)
         if key==ord('q'):
