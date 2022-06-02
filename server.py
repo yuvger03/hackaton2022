73d21fea2246047fd699c9517e91ebdfe87c8cf0
yuvger03 <yuvger03@gmail.com>
2022-06-02 21:30:48 +0300

    server - streets

--- server.py
+++ server.py
@@ -8,9 +8,6 @@ from brightness import getBrightness
 from street import voltage
 import threading
 
-streets = [r"C:\Users\yuvge\PycharmProjects\hackaton2022\street1",
-           r"C:\Users\yuvge\PycharmProjects\hackaton2022\street2"]
-
 
 class StreetThread(threading.Thread):
     def __init__(self, video):
@@ -32,9 +29,11 @@ class StreetThread(threading.Thread):
 
 
 if __name__ == '__main__':
+    streets = [r"C:\Users\yuvge\PycharmProjects\hackaton2022\street1",
+               r"C:\Users\yuvge\PycharmProjects\hackaton2022\street2"]
     for streetName in streets:
         videos = [str(join(streetName, f)) for f in listdir(streetName)
                   if (isfile(join(streetName, f)) and str(join(streetName, f)).split(".")[1] == "mp4")]
         print(videos)
-        thread = StreetThread(videos)  # threading.Thread(target=currStreet, args=(videos,))
+        thread = StreetThread(videos)
         thread.start()
